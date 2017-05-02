2dbd2ed5e3fe97fd3835b0ce61f56a388aed5e20
Lionel Adotevi <lionel@choupinette.localdomain>
2017-05-02 15:14:50 +0200

    Added enums files content :P

--- src/common/incidentEnums/CollisionType.ts
+++ src/common/incidentEnums/CollisionType.ts
@@ -1,3 +1,24 @@
 /**
  * Created by lionel on 02/05/17.
  */
+/**
+ * Contains Collision Type Values
+ */
+export enum CollisionType {
+    /**
+     * 1 – Deux véhicules - frontale
+     * 2 – Deux véhicules – par l’arrière
+     * 3 – Deux véhicules – par le coté
+     * 4 – Trois véhicules et plus – en chaîne
+     * 5 – Trois véhicules et plus  - collisions multiples
+     * 6 – Autre collision
+     * 7 – Sans collision
+     */
+    TwoCarsFrontal                = 1,
+    TwoCarsBackside               = 2,
+    TwoCarsOnSide                 = 3,
+    ThreeAndMoreInChain           = 4,
+    ThreeAndMoreMultipleCollision = 5,
+    Others                        = 6,
+    WithoutCollision              = 7
+}
--- src/common/incidentEnums/Luminosity.ts
+++ src/common/incidentEnums/Luminosity.ts
@@ -1,11 +1,20 @@
 /**
  * Created by lionel on 02/05/17.
  */
-
-export const enum Luminosity {
-    PLAINDAY,
-    DUSKORDAWN,
-    NIGHTWITHOUTLIGHTS,
-    NIGHTWITHLIGHTSOFF,
-    NIGHTWITHLIGHTSON
+/**
+ * Contains luminosity Values
+ */
+export enum Luminosity {
+    /**
+     * 1 – Plein jour
+     * 2 – Crépuscule ou aube
+     * 3 – Nuit sans éclairage public
+     * 4 - Nuit avec éclairage public non allumé
+     * 5 – Nuit avec éclairage public allumé
+     */
+    PlainDay           = 1,
+    DuskOrDawn         = 2,
+    NightWithoutLights = 3,
+    NightWithLightsOff = 4,
+    NightWithLightsOn  = 5
 }
