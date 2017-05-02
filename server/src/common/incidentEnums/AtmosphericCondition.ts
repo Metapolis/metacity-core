410190ee1b4c0385fd164b53ed721d0ad49cf5b2
Lionel Adotevi <lionel@choupinette.localdomain>
2017-05-02 15:14:50 +0200

    Added enums files content :P

--- server/src/common/incidentEnums/AtmosphericCondition.ts
+++ server/src/common/incidentEnums/AtmosphericCondition.ts
@@ -1,21 +1,28 @@
 /**
  * Created by lionel on 02/05/17.
  */
-/*
- 1 – Normale
- 2 – Pluie légère
- 3 – Pluie forte
- 4 – Neige - grêle
- 5 – Brouillard - fumée
- 6 – Vent fort - tempête
- 7 – Temps éblouissant
- 8 – Temps couvert
- 9 – Autre
+/**
+ * Contains atmospheric condition values
  */
-
-export enum AtmoConditions {
-    Normal,
-    LightRain,
-    HeavyRain,
-    SnowOr
+export enum AtmosphericCondition {
+    /**
+     * 1 – Normale
+     * 2 – Pluie légère
+     * 3 – Pluie forte
+     * 4 – Neige - grêle
+     * 5 – Brouillard - fumée
+     * 6 – Vent fort - tempête
+     * 7 – Temps éblouissant
+     * 8 – Temps couvert
+     * 9 – Autre
+     */
+    Normal          = 1,
+    LightRain       = 2,
+    HeavyRain       = 3,
+    SnowOrHail      = 4,
+    FogOrSmoke      = 5,
+    StrongWind      = 6,
+    DazzlingWeather = 7,
+    CloudyWeather   = 8,
+    Others          = 9
 }
--- server/src/common/incidentEnums/Intersection.ts
+++ server/src/common/incidentEnums/Intersection.ts
@@ -1,3 +1,28 @@
 /**
  * Created by lionel on 02/05/17.
  */
+/**
+ * Contains Intersection Values
+ */
+export enum Intersection {
+    /**
+     * 1 – Hors intersection
+     * 2 – Intersection en X
+     * 3 – Intersection en T
+     * 4 – Intersection en Y
+     * 5 - Intersection à plus de 4 branches
+     * 6 - Giratoire
+     * 7 - Place
+     * 8 – Passage à niveau
+     * 9 – Autre intersection
+     */
+    NoIntersection       = 1,
+    XIntersection        = 2,
+    TIntersection        = 3,
+    YIntersection        = 4,
+    FourPlusIntersection = 5,
+    Round                = 6,
+    Place                = 7,
+    RailroadCrossing     = 8,
+    Others               = 9
+}
