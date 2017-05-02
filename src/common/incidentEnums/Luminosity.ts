f4e6748a179d747d441b98c6e1cd54bced152f77
Lionel Adotevi <lionel@choupinette.localdomain>
2017-05-02 14:59:58 +0200

    Enums for accidents

---
+++ src/common/incidentEnums/Luminosity.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by lionel on 02/05/17.
+ */
+
+export const enum Luminosity {
+    PLAINDAY,
+    DUSKORDAWN,
+    NIGHTWITHOUTLIGHTS,
+    NIGHTWITHLIGHTSOFF,
+    NIGHTWITHLIGHTSON
+}
