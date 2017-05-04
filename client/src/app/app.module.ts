0f636331bc8db16bc1e98566b1d8e107237220ad
booleon <kde@florimond.eu>
2017-05-04 11:37:22 +0200

    Routing added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,10 +1,18 @@
+// Modules
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule }     from './app-routing.module';
 
+// Components
 import { AppComponent }  from './app.component';
 
+// Services
+
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [
+    BrowserModule,
+    AppRoutingModule
+  ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
