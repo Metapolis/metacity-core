cd92362efd040c2b5d6db15f06efaad92dbe69d9
booleon <kde@florimond.eu>
2017-05-04 17:10:02 +0200

    with new component file it's better

---
+++ client/src/app/graf-test/graf-test.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-graftest',
+  templateUrl: './graf-test.component.html',
+  styleUrls : ['./graf-test.component.css'],
+})
+export class GrafTestComponent { }
