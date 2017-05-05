799ac74168e9552306523ef3193fbbb5db41d7c2
Cedric Bonhomme <bonhomme@mailoo.org>
2017-05-05 14:08:09 +0200

    Map dependencies, call test

--- client/src/app/graf-test/graf-test.component.ts
+++ client/src/app/graf-test/graf-test.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { geo } from './map'
+import * as d3 from 'd3'
+import * as L from 'leaflet'
 
 @Component({
   selector: 'my-graftest',
   templateUrl: './graf-test.component.html',
-  styleUrls : ['./graf-test.component.css'],
+  styleUrls : ['./graf-test.component.css']
 })
-export class GrafTestComponent { }
+
+export class GrafTestComponent {
+  private map: geo.Map;
+
+  constructor() {
+    this.map = new geo.Map();
+  }
+
+  public draw(): void {
+    this.map.draw();
+  }
+}
---
+++ client/src/app/graf-test/map.ts
@@ -0,0 +1,15 @@
+export module geo {
+  /**
+   * Create a Map on a Svg element using leaflet
+   * @return {[type]} [description]
+   */
+  export class Map {
+
+    constructor() {
+    }
+
+    public draw(): void {
+      console.log("Drawing map in progress...");
+    }
+  }
+}
