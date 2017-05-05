637b4f7b7571c9f3e6cd3a61b4eef363d5c7c76f
booleon <kde@florimond.eu>
2017-05-05 21:24:30 +0200

    link of container 2 automated lets do the third container

--- client/src/app/home/container-2/container-2.component.ts
+++ client/src/app/home/container-2/container-2.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Assets }    from '../../assets';
+import { Link }      from '../../common/link';
 
 @Component({
   selector: 'container-2',
@@ -7,4 +8,21 @@ import { Assets }    from '../../assets';
 })
 export class Container2Component {
   assets = new Assets;
+  thematiqueslinks1 : Link[] = [
+    { src: "#", text: "Accessibilité"},
+    { src: "#", text: "Urbanisme"},
+    { src: "#", text: "Territoire"},
+    { src: "#", text: "Citoyenneté"},
+    { src: "#", text: "Économie"},
+    { src: "#", text: "Éducation & Jeunesse"},
+  ]
+  thematiqueslinks2 : Link[] = [
+    { src: "#", text: "Environnement"},
+    { src: "#", text: "Sports & Loisirs"},
+    { src: "#", text: "Santé"},
+    { src: "#", text: "Sécurité"},
+    { src: "#", text: "Mobilité"},
+    { src: "#", text: "Culture & Tourisme"},
+  ]
+
 }
