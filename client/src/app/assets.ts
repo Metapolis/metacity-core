840f9641363357dacfd092beec39db5459a34acb
Cedric Bonhomme <bonhomme@mailoo.org>
2017-05-12 10:51:48 +0200

    Dashboard assets 1/2

--- client/src/app/assets.ts
+++ client/src/app/assets.ts
@@ -5,4 +5,7 @@ export class Assets {
   homeIcon : Img = {src: "../assets/img/home.svg", alt: "Home icon"};
   metacityLogo : Img = {src: "../assets/img/metaCity.svg", alt: "metacity logo"};
   thematiques : Img = {src: "../assets/img/thematiquesimg.jpg", alt: "metacity logo"};
+  accidentsCarte: Img = {src: "../assets/img/accidents_carte.png", alt: "accidents map"};
+  victimes: Img = {src: "../assets/img/victimes.png", alt: "victimes"};
+  wordCloud: Img = {src: "../assets/img/motsclés.jpeg", alt: "word cloud "};
 }
--- client/src/app/dashboard/dbaccident/dbaccident.component.ts
+++ client/src/app/dashboard/dbaccident/dbaccident.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Assets } from '../../assets';
 
 @Component({
   selector: 'app-dbaccident',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dbaccident.component.scss', '../dashboard.component.scss']
 })
 export class DbaccidentComponent implements OnInit {
+  assets = new Assets;
 
   constructor() { }
 
