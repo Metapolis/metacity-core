02960ddcbe1e2e49d45cbe2dc605cb7f5c7900c0
Cedric Bonhomme <bonhomme@mailoo.org>
2017-05-15 14:17:27 +0200

    Home page pretty++;

--- client/src/app/home/container-1/container-1.component.ts
+++ client/src/app/home/container-1/container-1.component.ts
@@ -8,4 +8,14 @@ import { Assets }    from '../../assets';
 })
 export class Container1Component {
   assets = new Assets;
+  text = `MetaCity est une plateforme de collecte et d’échanges de données
+multi-domaines favorisant l’analyse, la visualisation et l’aide au pilotage
+des services et réseaux urbains. Cette plateforme est mise à
+disposition de tous les acteurs du territoire : citoyens, décideurs et
+opérateurs. En hébergeant et en fluidifiant la mise en commun des
+données nécessaires au fonctionnement des services de la ville, la
+plateforme MetaCity permet de partager la gouvernance territoriale et
+de faciliter l’implémentation et l’opération des services urbains, tout en
+plaçant le citoyen au coeur de la démarche smart city.
+`;
 }
--- client/src/app/home/container-3/container-3.component.ts
+++ client/src/app/home/container-3/container-3.component.ts
@@ -10,15 +10,53 @@ export class Container3Component {
   blocks: BlockContent[] = [
     {
       title: 'OPEN SOURCE',
-      text: 'MetaCity est un outil open source de gestion de données « smart » des territoires. La plateforme doit contribuer à faire émerger une communauté d’utilisateurs (villes, citoyens,entreprises privées ) et agir comme un  « tiers de confiance » pour favoriser la transparence et les échanges entre chacun.'
+      text: `MetaCity est un outil open
+source de gestion de
+données « smart » des
+territoires.La plateforme
+doit contribuer à faire
+émerger une
+communauté d’utilisateurs
+  (villes, citoyens, entreprises
+privées ) et agir comme un
+« tiers de confiance » pour
+favoriser la transparence
+et les échanges entre
+chacun.
+`
     },
     {
       title: 'INNOVANTE',
-      text: 'MetaCity est un outil open source de gestion de données « smart » des territoires. La plateforme doit contribuer à faire émerger une communauté d’utilisateurs (villes, citoyens,entreprises privées ) et agir comme un  « tiers de confiance » pour favoriser la transparence et les échanges entre chacun.'
+      text: `A travers l’ouverture et
+l’accès à plusieurs jeux de
+données, la plateforme
+MetaCity constitue un outil
+d’information et de
+transparence innovant, et
+permet de contribuer à
+l’open innovation en
+favorisant la ré - utilisation
+des données urbaines. Il
+s’agit également de
+favoriser l’adoption de
+standards de données et
+d’échanges permettant la
+généralisation de
+nouveaux services urbains
+`
     },
     {
       title: 'ÉVOLUTIVE',
-      text: 'MetaCity est un outil open source de gestion de données « smart » des territoires. La plateforme doit contribuer à faire émerger une communauté d’utilisateurs (villes, citoyens,entreprises privées ) et agir comme un  « tiers de confiance » pour favoriser la transparence et les échanges entre chacun.'
+      text: `La plateforme est pensée
+pour s’adapter aux
+évolutions des usages de
+la donnée urbaine, et
+anticiper l’ouverture
+croissante des flux de
+données issues des
+collectivités, des
+opérateurs et du
+`
     }
   ];
 }
