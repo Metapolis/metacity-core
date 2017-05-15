57f6aecd5f7f7dfd5e9e6a6d8dacfb0fef30912a
Cedric Bonhomme <bonhomme@mailoo.org>
2017-05-15 16:53:55 +0200

    TempleOS

--- client/src/app/assets.ts
+++ client/src/app/assets.ts
@@ -1,18 +1,19 @@
 import { Img } from './common/img';
 
 export class Assets {
-  whiteLogo : Img = {src: "../assets/img/metaCityBlanc.svg", alt: "metacity small logo"};
-  homeIcon : Img = {src: "../assets/img/home.svg", alt: "Home icon"};
-  metacityLogo : Img = {src: "../assets/img/metaCity.svg", alt: "metacity logo"};
-  thematiques : Img = {src: "../assets/img/thematiquesimg.jpg", alt: "metacity logo"};
-  accidentsCarte: Img = {src: "../assets/img/accidents_carte.png", alt: "accidents map"};
-  victimes: Img = {src: "../assets/img/victimes.png", alt: "victimes"};
-  wordCloud: Img = {src: "../assets/img/motsclés.jpeg", alt: "word cloud "};
-  presidentMap: Img = {src: "../assets/img/president_map.png", alt: ""};
-  presidentGraph: Img = {src: "../assets/img/president_graph.png", alt: ""};
-  legendGraph: Img = {src: "../assets/img/legend_graph.png", alt: ""};
-  home: Img = {src: "../assets/img/home.svg", alt: ""};
-  code: Img = {src: "../assets/img/code.svg", alt: ""};
-  search: Img = {src: "../assets/img/search.svg", alt: ""};
-  worldwide: Img = {src: "../assets/img/worldwide.svg", alt: ""};
+  whiteLogo: Img = {src: '../assets/img/metaCityBlanc.svg', alt: 'metacity small logo'};
+  homeIcon: Img = {src: '../assets/img/home.svg', alt: 'Home icon'};
+  metacityLogo: Img = {src: '../assets/img/metaCity.svg', alt: 'metacity logo'};
+  thematiques: Img = {src: '../assets/img/thematiquesimg.jpg', alt: 'metacity logo'};
+  accidentsCarte: Img = {src: '../assets/img/accidents_carte.png', alt: 'accidents map'};
+  victimes: Img = {src: '../assets/img/victimes.png', alt: 'victimes'};
+  wordCloud: Img = {src: '../assets/img/wordcloud.svg', alt: 'word cloud '};
+  presidentMap: Img = {src: '../assets/img/president_map.png', alt: ''};
+  presidentGraph: Img = {src: '../assets/img/president_graph.png', alt: ''};
+  legendGraph: Img = {src: '../assets/img/legend_graph.png', alt: ''};
+  home: Img = {src: '../assets/img/home.svg', alt: ''};
+  code: Img = {src: '../assets/img/code.svg', alt: ''};
+  search: Img = {src: '../assets/img/search.svg', alt: ''};
+  worldwide: Img = {src: '../assets/img/worldwide.svg', alt: ''};
+  world: Img = {src: '../assets/img/monde.png', alt: 'image statique Monde'};
 }
--- client/src/app/catalog/container-3/container-3.component.ts
+++ client/src/app/catalog/container-3/container-3.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BlockContent } from './block-content';
 import { Router, NavigationEnd } from '@angular/router';
+import { Assets }    from '../../assets';
 
 @Component({
   selector: 'container-3',
@@ -8,27 +9,28 @@ import { Router, NavigationEnd } from '@angular/router';
   styleUrls: ['../catalog.component.scss']
 })
 export class Container3Component implements OnInit {
-   constructor(private _router: Router) {}
+  assets = new Assets;
+  constructor(private _router: Router) { }
 
   public ngOnInit() {
     this._router.events.subscribe(val => {
       if (val instanceof NavigationEnd) {
-        (<any>window).twttr = (function (d, s, id) {
-            let js: any, fjs = d.getElementsByTagName(s)[0],
-                t = (<any>window).twttr || {};
-            if (d.getElementById(id)) return t;
-            js = d.createElement(s);
-            js.id = id;
-            js.src = 'https://platform.twitter.com/widgets.js';
-            fjs.parentNode.insertBefore(js, fjs);
+        (<any>window).twttr = (function(d, s, id) {
+          let js: any, fjs = d.getElementsByTagName(s)[0],
+            t = (<any>window).twttr || {};
+          if (d.getElementById(id)) return t;
+          js = d.createElement(s);
+          js.id = id;
+          js.src = 'https://platform.twitter.com/widgets.js';
+          fjs.parentNode.insertBefore(js, fjs);
 
-            t._e = [];
-            t.ready = function (f: any) {
-                t._e.push(f);
-            };
+          t._e = [];
+          t.ready = function(f: any) {
+            t._e.push(f);
+          };
 
-            return t;
-        }(document, 'script', 'twitter-wjs'));
+          return t;
+        } (document, 'script', 'twitter-wjs'));
       }
     });
   }
