b46d4404a51da49db698d6c36ae07887fa879c39
Florimond <florimond.boquet@metapolis.fr>
2017-05-17 14:44:18 +0200

    specific code of maps is now imported as a class. Need to transmorme as a service.

--- client/src/app/overview/map/map.component.ts
+++ client/src/app/overview/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MapSpecific } from './accident-map';
 
 import * as L from 'leaflet';
 import * as d3 from 'd3';
@@ -26,28 +27,16 @@ export class MapComponent implements OnInit {
     zoom: 13,
     center: L.latLng({ lat: 46.1621, lng: -1.1980 })
   };
+  mapspecific: MapSpecific;
 
   onMapReady(map: L.Map) {
-    const icon = {
-      icon: L.icon({
-        iconSize: [50, 50],
-        iconAnchor: [0, 0],
-        iconUrl: 'assets/markers.png',
-      })
-    };
-    d3.json('assets/mock-data/accidents.json', (err, data) => {
-
-      const pdata = data as Accident[];
-
-      pdata.forEach((item, index, array) => {
-          const lat_lon = [item.location.lat_lon[0] / 100000, item.location.lat_lon[1] / 100000] as L.LatLngExpression;
-          L.marker(lat_lon, icon).addTo(map);
-      });
-    });
+    this.mapspecific.onMapReady(map);
   }
 
 
-  constructor() { }
+  constructor() {
+    this.mapspecific = new MapSpecific;
+  }
 
   ngOnInit() {
   }
