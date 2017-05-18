365636ed16f393f7c4ab7bc646825963488e614e
lionel <lionel@metapolis.fr>
2017-05-18 20:13:20 +0200

    Epic Server Refacto

---
+++ src/controllers/rest/model/accident/Location.ts
@@ -0,0 +1,15 @@
+/**
+ * Contains accident location info
+ */
+export class Location {
+
+    /**
+     * Accident's address
+     */
+    public address: string;
+
+    /**
+     * Accident's latitude and longitude
+     */
+    public latLon: [number, number];
+}
