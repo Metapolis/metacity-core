de27c3d4e80f89cad4b516a20435bffca396acca
Florimond <florimond.boquet@metapolis.fr>
2017-05-19 09:40:22 +0200

    Small modification

--- client/src/app/overview/categories/accidents/map/accident-map.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: "app-accident-map",
-    templateUrl: 'accident-map.component.html',
-    styleUrls: ['../../../overview.component.scss']
-})
-export class AccidentMapComponent {}
