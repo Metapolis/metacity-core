331e9cb4369d62e5f3b5fb5e07f01c9bf84588a2
Florimond <florimond.boquet@metapolis.fr>
2017-05-29 17:40:26 +0200

    Green sendRequest works

--- client/src/app/overview/shared/http-request.service.ts
+++ client/src/app/overview/shared/http-request.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from "@angular/core";
+import { Injectable, ReflectiveInjector } from "@angular/core";
+import { async, fakeAsync, tick } from "@angular/core/testing";
+import { BaseRequestOptions, ConnectionBackend, Http, RequestOptions } from "@angular/http";
+import { Response, ResponseOptions } from "@angular/http";
 
 @Injectable()
 export class HttpRequestService {
   private serverAddress: string;
   private serverPort: number;
+
+  constructor(private http: Http) { }
 
   public setServerAddress(serverAddress: string) {
     this.serverAddress = serverAddress;
@@ -19,5 +24,14 @@ export class HttpRequestService {
 
   public getServerPort(): Promise<number> {
     return Promise.resolve(this.serverPort);
+  }
+
+  public sendRequest(request: string): void {
+    this.http.get(this.serverAddress + request);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error("An error occurred", error);
+    return Promise.reject(error.message || error);
   }
 }
