5a9145e85a4c7b5093881c7e4149caf01d471368
Florimond <florimond.boquet@metapolis.fr>
2017-05-29 15:01:06 +0200

    Set port test green

--- client/src/app/overview/shared/http-request.service.ts
+++ client/src/app/overview/shared/http-request.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class HttpRequestService {
   private serverAddress: string;
+  private serverPort: number;
 
   public setServerAddress(serverAddress: string) {
     this.serverAddress = serverAddress;
@@ -10,5 +11,13 @@ export class HttpRequestService {
 
   public getServerAddress(): Promise<string> {
     return Promise.resolve(this.serverAddress);
+  }
+
+  public setServerPort(serverPort: number) {
+    this.serverPort = serverPort;
+  }
+
+  public getServerPort(): Promise<number> {
+    return Promise.resolve(this.serverPort);
   }
 }
