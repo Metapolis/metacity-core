4447ff3668779e384bbd5d29d870670341bb6955
Florimond <florimond.boquet@metapolis.fr>
2017-06-02 16:15:17 +0200

    Ready for mock data Bordeaux. Server arrive at next merge

--- client/src/app/overview/map/contents/accident-map-content.ts
+++ client/src/app/overview/map/contents/accident-map-content.ts
@@ -4,6 +4,7 @@ import * as fmt from "sprintf-js";
 
 import { HttpRequestService } from "../../../shared/http-request.service";
 import { RequestForm } from "../../../common/request-form";
+import * as mockData from "./mock-accidents-data";
 
 interface InitialBoundsInterface {
   northWest: L.LatLng;
@@ -78,63 +79,64 @@ export class AccidentMapContent implements MapContent {
     const tempRequest: RequestForm = {
       path: "traffics/accidents",
       params: [
-        {key: "limit", value: "25"},
-        {key: "offset", value: "50"},
-        {key: "areas", value: "[[13.0|53.0]|[14.0|52.0]],[[11.0|55.0]|[12.0|54.0]]"}
+        { key: "limit", value: "25" },
+        { key: "offset", value: "50" },
+        { key: "areas", value: "[[13.0|53.0]|[14.0|52.0]],[[11.0|55.0]|[12.0|54.0]]" }
       ]
     };
-    this.httpRequestService.getRequestData(tempRequest)
-      .then((answer: string) => { this.draw(answer); } )
-      .catch((err: any) => { alert("Data not available ==> " + err); });
+    // waiting for a real server communication we will continue to use mock data
+    // TODO: Use data from server again
+    // this.httpRequestService.getRequestData(tempRequest)
+    //   .then((answer: string) => { this.draw(answer); } )
+    //   .catch((err: any) => { alert("Data not available ==> " + err); });
+    this.draw(mockData.MOCKACCIDENTDATA);
   }
 
   public draw(mapContent: string): void {
-    console.log(JSON.parse(mapContent).results[0]);
-    d3.json(JSON.parse(mapContent).results, (err, data) => {
+    const data = JSON.parse(mapContent);
 
-      const pdata = data as Array<{
-        id: number,
-        location: { address: string, lat_lon: L.LatLngExpression },
-        climatology: { atmosphericCondition: number, luminosity: number },
-        collisionType: number
-      }>;
+    const pdata = data as Array<{
+      id: number,
+      location: { address: string, lat_lon: L.LatLngExpression },
+      climatology: { atmosphericCondition: number, luminosity: number },
+      collisionType: number
+    }>;
 
-      pdata.forEach((item, index, array) => {
-        if (this.weatherFilters.indexOf(item.climatology.atmosphericCondition) >= 0) {
-          const lat_lon = [item.location.lat_lon[0] / ONE_HUNDRED_THOUSAND, item.location.lat_lon[1] / ONE_HUNDRED_THOUSAND] as L.LatLngExpression;
-          const layer = L.marker(lat_lon, this.icon);
-          layer.bindPopup(
-            "<h6>" + item.location.address + "</h6>" +
-            "<hr>" +
-            "<b>meteo</b>: " + this.weatherFiltersMap[item.climatology.atmosphericCondition] +
-            "<br>" +
-            "<b>type de collision</b>: " + this.collisionMap[item.collisionType]
-          );
-          this.layers.push(layer);
-        }
-      });
+    pdata.forEach((item, index, array) => {
+      if (this.weatherFilters.indexOf(item.climatology.atmosphericCondition) >= 0) {
+        const lat_lon = [item.location.lat_lon[0] / ONE_HUNDRED_THOUSAND, item.location.lat_lon[1] / ONE_HUNDRED_THOUSAND] as L.LatLngExpression;
+        const layer = L.marker(lat_lon, this.icon);
+        layer.bindPopup(
+          "<h6>" + item.location.address + "</h6>" +
+          "<hr>" +
+          "<b>meteo</b>: " + this.weatherFiltersMap[item.climatology.atmosphericCondition] +
+          "<br>" +
+          "<b>type de collision</b>: " + this.collisionMap[item.collisionType]
+        );
+        this.layers.push(layer);
+      }
+    });
 
-      this.layerGroup = L.layerGroup(this.layers);
-      this.layerGroup.addTo(this.map);
+    this.layerGroup = L.layerGroup(this.layers);
+    this.layerGroup.addTo(this.map);
 
-      this.map.on("movestart", () => {
-        console.log("Move start event fired");
-        const bounds: L.LatLngBounds = this.map.getBounds();
-        const nw: L.LatLng = bounds.getNorthWest();
-        const se: L.LatLng = bounds.getSouthEast();
+    this.map.on("movestart", () => {
+      console.log("Move start event fired");
+      const bounds: L.LatLngBounds = this.map.getBounds();
+      const nw: L.LatLng = bounds.getNorthWest();
+      const se: L.LatLng = bounds.getSouthEast();
 
-        const bounds_query = [
-          [nw.lat, nw.lng],
-          [se.lat, se.lng]
-        ];
-        const bounds_query_string: string = fmt.sprintf("[[%f|%f][%f|%f]]", nw.lat, nw.lng, se.lat, se.lng);
-        console.log(bounds_query_string);
-      });
+      const bounds_query = [
+        [nw.lat, nw.lng],
+        [se.lat, se.lng]
+      ];
+      const bounds_query_string: string = fmt.sprintf("[[%f|%f][%f|%f]]", nw.lat, nw.lng, se.lat, se.lng);
+      console.log(bounds_query_string);
+    });
 
-      this.map.on("moveend", () => {
-        console.log("Move end event fired");
-        // Refresh data
-      });
+    this.map.on("moveend", () => {
+      console.log("Move end event fired");
+      // Refresh data
     });
   }
 }
