e5ce4384c05165ea33fefe5203ee5bab12fc0ef5
Mathieu Bayou <mathieu.bayou@metapolis.fr>
2017-06-14 15:56:52 +0200

    Add treatment for tweet filter in service, have to be tested

--- src/services/query/impl/TweetQueryServiceImpl.ts
+++ src/services/query/impl/TweetQueryServiceImpl.ts
@@ -6,6 +6,9 @@ import { TweetDTO } from "../dto/tweet/TweetDTO";
 import { Client } from "elasticsearch";
 import { LoggerInstance } from "winston";
 import { Utils } from "../../../common/Utils";
+import { QueryBuilder } from "../builder/elasticsearch/QueryBuilder";
+import { RangeQueryParam } from "../builder/elasticsearch/model/RangeQueryParam";
+import { TermQueryParam } from "../builder/elasticsearch/model/TermQueryParam";
 
 /**
  * Implementation of {@link TrafficQueryService}
@@ -28,17 +31,65 @@ export class TweetQueryServiceImpl implements TweetQueryService {
 
     /**
      * Override
+     *
      * @param query
      */
     public async findTweets(query: FindTweetQuery): Promise<ResultList<TweetDTO>> {
         this.logger.info("Retrieve all traffic accident in elastic search");
+
+        // Create query builder
+        let queryBuilder: QueryBuilder;
+        if (query.isSet()) {
+            queryBuilder = new QueryBuilder();
+
+            // Create date range filter
+            if (query.getDateFilter() !== null) {
+                for (const dateRange of query.getDateFilter().getMustParams()) {
+                    const rangeQuery: RangeQueryParam = new RangeQueryParam();
+                    rangeQuery.field = "createdAt";
+                    rangeQuery.createGTELTNumber(dateRange.getStart(), dateRange.getEnd());
+                    queryBuilder.must(rangeQuery);
+                }
+                for (const dateRange of query.getDateFilter().getShouldParams()) {
+                    const rangeQuery: RangeQueryParam = new RangeQueryParam();
+                    rangeQuery.field = "createdAt";
+                    rangeQuery.createGTELTNumber(dateRange.getStart(), dateRange.getEnd());
+                    queryBuilder.should(rangeQuery);
+                }
+            }
+
+            // Create mention filter
+            if (query.getMentionFilter() !== null) {
+                for (const mention of query.getMentionFilter().getMustParams()) {
+                    queryBuilder.must(new TermQueryParam("mentions", mention));
+                }
+                for (const mention of query.getMentionFilter().getShouldParams()) {
+                    queryBuilder.should(new TermQueryParam("mentions", mention));
+                }
+            }
+
+            // Create hashtag filter
+            if (query.getHashtagFilter() !== null) {
+                for (const hashtag of query.getHashtagFilter().getMustParams()) {
+                    queryBuilder.must(new TermQueryParam("mentions", hashtag));
+                }
+                for (const hashtag of query.getHashtagFilter().getShouldParams()) {
+                    queryBuilder.should(new TermQueryParam("mentions", hashtag));
+                }
+            }
+            this.logger.info("Query elastic : '%s'", queryBuilder.build());
+        }
+
+        // Call elastic with query
         const jsonTweets = (await this.esClient.search({
             index: query.getIndex(),
             type: query.getType(),
             size: query.getLimit(),
-            from: query.getOffset()
+            from: query.getOffset(),
+            body: queryBuilder.build()
         })).hits;
 
+        // Parse result to DTO
         const accidents: TweetDTO[] = [];
         for (const jsonTweet of jsonTweets.hits) {
             accidents.push(new TweetDTO(jsonTweet._source as { type: string, category: string }));
