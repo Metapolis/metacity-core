1ab71eb17e73c3c1e82cce037eab342b05b92aec
Mathieu Bayou <mathieu.bayou@metapolis.fr>
2017-07-31 13:46:24 +0200

    add documentation to decorator and use service

--- src/common/Decorators.ts
+++ src/common/Decorators.ts
@@ -7,7 +7,9 @@ import { ContextApp } from "../ContextApp";
 import { CollectivityDao } from "../persistence/dao/CollectivityDao";
 import { User } from "../persistence/domain/User";
 import { UserDao } from "../persistence/dao/UserDao";
-import { JWTPayload } from "./JWTToken";
+import { JWTPayload } from "./security/JWTToken";
+import { SecurityManager } from "./security/SecurityManager";
+
 /**
  * Check if user can access to resource
  * @param roles
@@ -18,23 +20,18 @@ function Secured(roles: string[]) {
     return (target: object, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) => {
         const originalMethod = descriptor.value;
 
-        descriptor.value = async function (...args: any[]) {
-            const domain: string = RequestAccessor.getRequest().hostname.split(".")[0];
-            const collectivityDao: CollectivityDao = ContextApp.getContainer().get("CollectivityDao") as CollectivityDao;
+        // Add code before execute method
+        descriptor.value = async function(...args: any[]) {
             const userDao: UserDao = ContextApp.getContainer().get("UserDao") as UserDao;
 
-            const collectivity: Collectivity = await collectivityDao.findById(domain);
-            if (collectivity === undefined) {
-                this.logger.error("Collectivity not found");
-                throw new AccessDeniedError("Access denied");
-            }
-
+            // retrive the authorization in header
             const authorizationRaw: string = RequestAccessor.getRequest().headers.authorization;
             if (authorizationRaw === undefined) {
                 this.logger.error("Token not found");
                 throw new AccessDeniedError("No token found");
             }
 
+            // Split authorization value to retrieve the jwt value
             const authorizationArray: string[] = authorizationRaw.split(" ");
             Utils.checkArgument(authorizationArray.length > 1, "Malformed authentication method");
 
@@ -47,7 +44,11 @@ function Secured(roles: string[]) {
             const jwtEncoded = authorizationArray[1];
             Utils.checkArgument(!Utils.isNullOrEmpty(jwtEncoded), "Malformed token");
 
-            const authorizationElement: JWTPayload = (JWT.verify(jwtEncoded, collectivity.getSecret()) as JWTPayload);
+            // Retrieve the sub domain who is the identifier of collectivity
+            const domain: string = RequestAccessor.getRequest().hostname.split(".")[0];
+
+            // Retrieve the payload value
+            const authorizationElement: JWTPayload = await (ContextApp.getContainer().get("SecurityManager") as SecurityManager).authenticate(domain, jwtEncoded);
 
             const user: User = await userDao.findById(authorizationElement.id);
 
@@ -56,11 +57,7 @@ function Secured(roles: string[]) {
                 throw new AccessDeniedError("Access denied");
             }
 
-            let userRoles: string[] = [];
-            for (const circle of await user.getCircles()) {
-                userRoles = userRoles.concat(circle.getRoles());
-            }
-
+            const userRoles: string[] = await user.getRoles();
             for (const role of roles) {
                 if (userRoles.indexOf(role) === -1) {
                     this.logger.error("User cannot access to this resource");
---
+++ src/common/security/SecurityManager.ts
@@ -0,0 +1,3 @@
+/**
+ * Created by mbayou on 27/07/2017.
+ */
