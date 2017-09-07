71b1cdb85b9d92749533ea8c3f3003e59cb6420f
Nicolas <nicolas.jellab@metapolis.fr>
2017-09-07 10:45:00 +0200

    CircleCommandServiceTest v1

--- test/services/command/CircleCommandServiceTest.ts
+++ test/services/command/CircleCommandServiceTest.ts
@@ -1,30 +1,57 @@
 import { AbstractTestService } from "../inversify/AbstractTestService";
+import { suite, test } from "mocha-typescript";
+import { Client, GetParams } from "elasticsearch";
+import * as Request from "request-promise";
+import * as Chai from "chai";
+import { ContextApp} from "../../../src/ContextApp";
+import * as TypeMoq from "typemoq";
+import * as HTTPStatusCodes from "http-status-codes";
+import {  } from "../../src/persistence/domain/ActivityCircle";
+import {isNullOrUndefined} from "util";
+import {ActivityCircle} from "../../../src/persistence/domain/ActivityCircle";
+import { SaveCircleCommandDTO } from "../../../src/services/command/dto/circles/SaveCircleCommandDTO";
+import {IllegalArgumentError} from "../../../src/common/error/IllegalArgumentError";
+import {CircleCommandService} from "../../../src/services/command/CircleCommandService";
+import {Collectivity} from "../../../src/persistence/domain/Collectivity";
+import {ObjectType} from "typeorm";
 
 /**
  * All test for user collectivity query service
  */
-//@suite
-class CircleCommandServiceTest extends AbstractTestService{
+// @suite
+class CircleCommandServiceTest extends AbstractTestService {
 
-    //@test
+    // @test
     private async testCreateCircle(): Promise<void> {
-        /*const circleCommandService: CircleCommandService = (ContextApp.container.get("CircleCommandService") as CircleCommandService);
-        const circleDaoMock: TypeMoq.IMock<CircleDao> = (ContextApp.container.get("CircleDaoMock") as TypeMoq.IMock<CircleDao>);
+        // test if circle is saved in Dao
+        // test if circle id is correct
+        // test if collectivity exists
+        // verify parameters name and role
 
+        const accessKey: string = "starkindustries";
+        const circleIdentifier: number = 1;
+        const collectivity: Collectivity = new Collectivity();
+        const circleCommandService: TypeMoq.IMock<CircleCommandService> = (ContextApp.container.get("CircleCommandServiceMock") as TypeMoq.IMock<CircleCommandService>);
+        const esClient: TypeMoq.IMock<Client> = (ContextApp.container.get("ESClientMock") as TypeMoq.IMock<Client>);
 
-       /!* const activityCircleMock: ActivityCircle = new ActivityCircle();
-        activityCircleMock.setRoles(["admin"]);
-        activityCircleMock.setName("Champion");
-        *!/
+        const circle: ActivityCircle = new ActivityCircle();
+        circle.setName("Michel");
+        circle.setId(1);
+        circle.setRoles(["Champion"]);
+        circle.setCollectivity(collectivity);
 
-        const saveCircleCommandDTO: SaveCircleCommandDTO = new SaveCircleCommandDTO();
-        saveCircleCommandDTO.setName("Champion");
-        saveCircleCommandDTO.setRoles(["admin"]);
-        saveCircleCommandDTO.setAccessKey("accesskey");
+        esClient.verify((instance) => instance.search(TypeMoq.It.is((getParams: GetParams) => {
+            let ret: boolean = getParams.type ==== ObjectType
+        }
 
-        circleCreationMock.setup((instance) => instance.createCircle("localhost")).returns(() => Promise.resolve(activityCircleMock));
+        circleCommandService.setup((instance: CircleCommandService) => instance.createCircle(TypeMoq.It.is((collectivityCircle: SaveCircleCommandDTO) => {
+            let ret = collectivityCircle.getName() === circle.getName();
+            for (let i = 0; i < circle.getRoles().length; i++) {
+                ret = ret && collectivityCircle.getRoles()[i] === circle.getRoles()[i];
+            }
+            ret = ret && collectivityCircle.getAccessKey() === accessKey;
+            return ret;
+        }))).returns(() => Promise.resolve(circleIdentifier);
 
-        // const circleDTO: SaveCircleCommandDTO = await circleCommandService.createCircle(saveCircleCommandDTO);
-Chai.assert.equal(, activityCircleMock.getId());*/
     }
 }
