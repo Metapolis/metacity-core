989e2938e3d5630d02cc2c0b4380e506a81c83f3
Nicolas <nicolas.jellab@metapolis.fr>
2017-10-12 11:00:32 +0200

    fix pull request

--- src/services/command/dto/circle/UpdateCircleCommandDTO.ts
+++ src/services/command/dto/circle/UpdateCircleCommandDTO.ts
@@ -120,18 +120,18 @@ export class UpdateCircleCommandDTO {
     /**
      * Get members for all fields
      *
-     * @returns {SaveUserCommandDTO[]} members array of members
+     * @returns {number[]} members array of members
      */
-    public getMembers(): SaveUserCommandDTO[] {
+    public getMembers(): number[] {
         return this.members;
     }
 
     /**
      * Set members for all fields
      *
-     * @param {SaveUserCommandDTO} members array of members
+     * @param {number[]} members array of members
      */
-    public setMembers(members: SaveUserCommandDTO[]): void {
+    public setMembers(members: number[]): void {
         this.members = members;
     }
 }
